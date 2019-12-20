ec18dd291d886d15c8beb2932822fbe1f779a7b7
bayleyjensen <bayley3jensen0@gmail.com>
2019-12-20 15:31:11 -0700

    server spins up wit bug cont and serv are squished

---
+++ server/controllers/BugController.js
@@ -0,0 +1,58 @@
+import express from "express";
+import bugService from "../services/BugService";
+
+export default class BugController {
+  constructor() {
+    this.router = express
+      .Router({ mergeParams: true })
+      //NOTE  each route gets registered as a .get, .post, .put, or .delete, the first parameter of each method is a string to be concatinated onto the base url registered with the route in main. The second parameter is the method that will be run when this route is hit.
+      .get("", this.getAll)
+      .get("/:id", this.getById)
+      .post("", this.create) //api/cars
+      .put("/:id", this.edit)
+      .delete("/:id", this.delete);
+  }
+
+  async getAll(req, res, next) {
+    try {
+      let data = await bugService.getAll();
+      return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async getById(req, res, next) {
+    try {
+      let data = await bugService.getById(req.params.id);
+      return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async create(req, res, next) {
+    try {
+      let data = await bugService.create(req.body);
+      return res.status(201).send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      let data = await bugService.edit(req.params.id, req.body);
+      return res.send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+      await bugService.delete(req.params.id);
+      return res.send("Successfully Deleted");
+    } catch (error) {
+      next(error);
+    }
+  }
+}
